23b5140740bec6294e83ce61359bb9b4ed911518
Brittney Jones <brittney.r.jones519@gmail.com>
2023-04-05 23:51:53 +0000

    implementing create activity

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -160,7 +160,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'bj'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
@@ -206,7 +206,7 @@ def rollbar_test():
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-  user_handle  = 'andrewbrown'
+  user_handle  = 'bj'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
   if model['errors'] is not None:
